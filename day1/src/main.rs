a5734c1212616c6ae0484440a22b8a6d15e01cc5
Jošt Stergar <jost.stergar@fmf.uni-lj.si>
2021-12-02 14:54:59 +0100

    Improved exception handling in day1.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,46 +3,48 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    // Store for task 1
     let mut prev = 0;
     let mut larger = -1;
+    // Store for task 2
     let mut i = 0;
     let mut largerf = -1;
     let mut prevf = 0;
     let mut window: [i32; 3] = [0; 3];
 
     // File input must exist in current path before this produces output
-    if let Ok(lines) = read_lines("./input") {
+    if let Result::Ok(lines) = read_lines("./input") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
-            if let Ok(depth) = line {
-                // Get current depth
-                let current = depth.parse::<i32>().unwrap();
-                
-                // With window
-                if i < 2{
-                    // in first two iterations just fill the window
-                    window[i%3] = current;
-                } else {
-                    window[i%3] = current;
-                    let currentf = window[0] + window[1] + window[2];
-                    if currentf > prevf {
-                        largerf += 1;
-                    }
-                    prevf = currentf;
+            // Get current depth
+            let current = line.unwrap().parse::<i32>().unwrap();
+            
+            // With window
+            if i < 2{
+                // in first two iterations just fill the window
+                window[i%3] = current;
+            } else {
+                window[i%3] = current;
+                let currentf = window[0] + window[1] + window[2];
+                if currentf > prevf {
+                    largerf += 1;
                 }
-                i += 1;
-
-                // Without window
-                if current > prev{
-                    larger += 1;
-                }
-                prev = current;
+                prevf = currentf;
             }
-        }
-    }
+            i += 1;
 
-    println!("Result is: {}", larger);
-    println!("Filtered result is: {}", largerf);
+            // Without window
+            if current > prev{
+                larger += 1;
+            }
+            prev = current;
+        }
+        println!("Result is: {}", larger);
+        println!("Filtered result is: {}", largerf);
+    } else if let Result::Err(e) = read_lines("./input"){
+        println!("Input file does not exist!");
+        println!("{:?}", e);
+    }
 }
 
 // The output is wrapped in a Result to allow matching on errors
@@ -50,5 +52,5 @@ fn main() {
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Result::Ok(io::BufReader::new(file).lines())
 }
