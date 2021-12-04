253b4029a29c847878f370ca6ca9e87e2dd724e9
Jošt Stergar <jost.stergar@fmf.uni-lj.si>
2021-12-04 15:11:21 +0100

    Day 4, pt 1 works!

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -4,12 +4,76 @@ use std::path::Path;
 
 fn main() {
     // We will read the data into these two mutable variables
+    static SIZE: u32 = 5;
     let mut nums: Vec<u32> = Vec::<u32>::new();
     let mut boards: Vec<BingoBoard> = Vec::<BingoBoard>::new();
 
     match read_lines("./input"){
         Result::Ok(lines) => {
-            // Iterate through file
+            // Create temporary board that we will read line by line
+            let mut board = BingoBoard::new(SIZE);
+            let mut idx = 0; // This is the new board row index
+
+            // Iterate through file and read file into structures
+            for (i, line) in lines.enumerate() {
+                let string = line.expect("No line!");
+                // For first line we read a string and fill it into nums
+                if i == 0 {
+                    string
+                        .split(",")
+                        .for_each(|x| nums.push(x.parse::<u32>().expect("Not a number")));
+                    continue;
+                }
+
+                // Completely skip the first line
+                if i == 1 {continue;}
+                
+                // If this is an empty line and not the first, push the board and initialize a new one
+                if string.chars().count() == 0 && i != 1{
+                    board.order();
+                    boards.push(board);
+                    board = BingoBoard::new(SIZE);
+                    idx = 0;
+                    continue;
+                }
+
+                // If we got to here, fill the board!
+                string
+                    .split_whitespace()
+                    .enumerate()
+                    .for_each(|(j, x)| {
+                        board.add(BingoNumber{
+                            x: j,
+                            y: idx,
+                            num: x.parse::<u32>().expect("Not a number"),
+                            ..Default::default()
+                        })
+                    });
+                // Increase the row index
+                idx += 1;
+            }
+
+            // Now that we have the data, let's play BINGO!
+            
+            // Part one
+            let mut score: u32 = 0;
+            let mut num: u32 = 0;
+            'outer: for n in nums {
+                for i in 0..boards.len() {
+                    boards[i].call(n);
+                    if boards[i].test() {
+                        score = boards[i].score();
+                        num = n;
+                        break 'outer;
+                    }
+                }
+            }
+            println!("Sum is: {} and number is {}", score, num);
+            println!("Answer is thus: {}", score*num);
+
+            // Part two
+            score = 0;
+            num = 0;
             
         },
         Result::Err(e) => {
@@ -20,23 +84,12 @@ fn main() {
 }
 
 // This is the data of a single number on the board
+#[derive(Default)]
 struct BingoNumber{
     num: u32,
     x: usize,
     y: usize,
     xed: bool,
-}
-
-// Default values for the BingoNumber struct
-impl Default for BingoNumber {
-    fn default() -> BingoNumber {
-        BingoNumber {
-            num: 0,
-            x: 0,
-            y: 0,
-            xed: false,
-        }
-    }
 }
 
 // This are methods for the board
@@ -71,7 +124,7 @@ impl BingoBoard {
 // Implement Bingo on BingoBoard
 impl Bingo for BingoBoard {
     fn call(&mut self, c: u32) -> (){
-        // TODO this is ugly and slow O(n2)
+        // TODO: this is ugly and slow O(n2)
         for i in 0..self.nums.len() {
             if c == self.nums[i].num {
                 if !self.nums[i].xed{
