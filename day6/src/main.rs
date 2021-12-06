df44070d99118c3b07d640a0999d87d679819c06
Jošt Stergar <jost.stergar@fmf.uni-lj.si>
2021-12-06 12:19:50 +0100

    Tidied the code for day 6 slightly.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -10,45 +10,59 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+// Define number of classes
+const CLASSNUM:usize = 9;
+
+/// Program entry point
 fn main() {
-    if let Ok(mut ages) = parse_input("./input"){
-        let mut groups: [u64; 9] = [0;9];
-        // Parse groups for day 0
-        for i in 0..9 {
-            groups[i] = ages.iter().fold(0, |acc, &x| acc + if x==i as i32 {1} else {0})
-        }
-
-        // PART ONE
-        for i in 0..80 {
-            // First check how many children we need to add at the end
-            let spawn: i32 = ages.iter().fold(0, |acc, &x| acc + if x == 0 {1} else {0});
-            
-            // Decrement ages
-            ages = ages.iter().map(|&x| if x == 0 {6} else {x-1}).collect();
-
-            // Spawn offspring
-            ages.append(&mut vec![8;spawn as usize]);
-        }
-
-        // PART TWO
-        for i in 0..256 {
-            // Check how many spawns we need
-            let spawn: u64 = groups[0];
-            // Decrement ages
-            for k in 0..8 {
-                groups[k] = groups[k+1];
-            }
-            // Move parents to group 6
-            groups[6] += spawn;
-            // Spawn new
-            groups[8] = spawn;
-        }
-
+    if let Ok(ages) = parse_input("./input"){
         println!("PART ONE:");
-        println!("Number after 80 days: {}", ages.len());
+        println!("Number after 80 days: {}", naive_solution(&ages, 80));
         println!("PART TWO:");
-        println!("Number afte 256 days: {}", groups[0]+groups[1]+groups[2]+groups[3]+groups[4]+groups[5]+groups[6]+groups[7]+groups[8]);
+        println!("Number afte 256 days: {}", better_solution(&ages, 256));
     }
+}
+
+/// This is a better solution that uses age groups as cache
+fn better_solution(agesb: &Vec<i32>, epochs: i32) -> u64 {
+    // Variables used by the function
+    let ages = agesb.to_owned();
+    let mut groups: [u64; CLASSNUM] = [0; CLASSNUM];
+
+    // Parse groups for day 0
+    for i in 0..CLASSNUM {
+        groups[i] = ages.iter().fold(0, |acc, &x| acc + if x==i as i32 {1} else {0})
+    }
+
+    for _ in 0..epochs {
+        // Check how many spawns we need
+        let spawn: u64 = groups[0];
+        // Decrement ages
+        for k in 0..CLASSNUM-1 {
+            groups[k] = groups[k+1];
+        }
+        // Move parents to group 6
+        groups[6] += spawn;
+        // Spawn new
+        groups[CLASSNUM-1] = spawn;
+    }
+
+    groups.iter().sum::<u64>()
+}
+
+/// This is a naive solution that uses a list of ages as cache
+fn naive_solution(agesb: &Vec<i32>, epochs: i32) -> i32{
+    let mut ages = agesb.to_owned();
+    // PART ONE
+    for _ in 0..epochs {
+        // First check how many children we need to add at the end
+        let spawn: i32 = ages.iter().fold(0, |acc, &x| acc + if x == 0 {1} else {0});
+        // Decrement ages
+        ages = ages.iter().map(|&x| if x == 0 {6} else {x-1}).collect();
+        // Spawn offspring
+        ages.append(&mut vec![8;spawn as usize]);
+    }
+    ages.len() as i32
 }
 
 /// This returns a vector of ages, as given in the input filename and wrapped in Result
