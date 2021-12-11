a7ea22d83a0bd2859d17ea74bcb75a24d2a95e2e
Jošt Stergar <jost.stergar@fmf.uni-lj.si>
2021-12-11 08:52:38 +0100

    Added source tree, inputs and parser for day11

---
+++ day11/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(data) = parse_input("./input") {
+        println!("{:?}", data);
+    }
+}
+
+
+/// This returns a matrix of energy levels, as given in the input filename and wrapped in Result
+fn parse_input(filename: &str) -> io::Result<Vec<Vec<i32>>>{
+    let mut data: Vec<Vec<i32>> = Vec::new();
+    match read_lines(filename){
+        Result::Ok(lines) => {            // Parse input lines
+            for line in lines {
+                data.push(line.expect("Invalid line")
+                                .chars()
+                                .map(|x| x.to_string().parse::<i32>().expect("Cannot parse!"))
+                                .collect::<Vec<i32>>());
+            }
+            return Ok(data);
+        },
+        Result::Err(e) => {
+            println!("File error!");
+            println!("{:?}", e);
+            return Err(e);
+        },
+    };
+}
+
+/// This is a buffered file reader which opens a file at filename and resturns io::Result
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Result::Ok(io::BufReader::new(file).lines())
+}
