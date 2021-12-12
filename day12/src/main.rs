867738b824612a5c9ebb0ffcbd6dcb9671fef28b
Jošt Stergar <jost.stergar@fmf.uni-lj.si>
2021-12-12 11:37:44 +0100

    Pt2 of day12 works but is VERY UGLY!

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -9,8 +9,104 @@ use std::path::Path;
 use std::collections::HashMap;
 
 fn main() {
-    if let Ok(data) = parse_input("./test1") {
-        println!("{:?}", data);
+    if let Ok(data) = parse_input("./input") {
+        println!("PART ONE:");
+        println!("Paths found: {}", part_one(&data));
+        println!("PART TWO:");
+        println!("Paths found: {}", part_two(&data));
+    }
+}
+
+fn part_two(data: &HashMap<String, Vec<String>>) -> i32 {
+    let mut paths = vec![vec!["start".to_string()]];
+    find_paths_two(&mut paths, &data);
+    //for path in paths.clone() {println!("{:?}", path);};
+    paths.len() as i32
+}
+
+fn find_paths_two(paths: &mut Vec<Vec<String>>, data: &HashMap<String, Vec<String>>) -> (){
+    loop {
+        let mut changes = 0;
+        let mut i = 0;
+        // Create a copy of previous iteration and empty paths
+        let tmp = paths.clone();
+        paths.retain(|_| false);
+
+        // Now iterate over tmp and expand paths. Write new paths into paths and 
+        // increment counter
+        for path in tmp.clone() {
+            // Check if we are at the end, in this case, just store path into output do not do anything
+            if path.clone().last().unwrap() == "end" {
+                paths.push(path);
+            } else {
+                let points: &Vec<String> = data.get(path.clone().last().unwrap()).unwrap();
+                // count existing repetitions
+                let mut repeat_old = 0;
+                for pt in path.clone() {
+                    repeat_old += path.clone().iter().fold(0, |acc, x| if x == &pt && x!="start" && x.chars().all(|c| matches!(c, 'a'..='z')) {acc + 1} else {acc});
+                }
+                repeat_old -= path.clone().iter().filter(|&x| x!="start" && x.chars().all(|c| matches!(c, 'a'..='z'))).count();
+                for point in points.clone(){
+                    // add "future" repetitions if any
+                    let repeat_new= path.clone().iter().fold(0, |acc, x| if x == &point && x!="start" && x.chars().all(|c| matches!(c, 'a'..='z')) {acc + 1} else {acc});
+                    if  repeat_new + repeat_old <= 2 && point != "start" {
+                        changes += 1;
+                        let mut new_path = path.clone();
+                        new_path.push(point);
+                        paths.push(new_path);
+                    }
+                }
+            }
+            i += 1;
+        }
+
+        // Break if no changes done in this iteration and place tmp back into paths
+        if changes == 0 {
+            //paths.append(&mut tmp);
+            break;
+        };
+    }
+}
+
+fn part_one(data: &HashMap<String, Vec<String>>) -> i32 {
+    let mut paths = vec![vec!["start".to_string()]];
+    find_paths(&mut paths, &data);
+    //for path in paths.clone() {println!("{:?}", path);};
+    paths.len() as i32
+}
+
+fn find_paths(paths: &mut Vec<Vec<String>>, data: &HashMap<String, Vec<String>>) -> (){
+    loop {
+        let mut changes = 0;
+        // Create a copy of previous iteration and empty paths
+        let tmp = paths.clone();
+        paths.retain(|_| false);
+
+        // Now iterate over tmp and expand paths. Write new paths into paths and 
+        // increment counter
+        for path in tmp.clone() {
+            // Check if we are at the end, in this case, just store path into output do not do anything
+            if path.clone().last().unwrap() == "end" {
+                paths.push(path);
+            } else {
+                let points: &Vec<String> = data.get(path.clone().last().unwrap()).unwrap();
+                for point in points.clone(){
+                    // check that point is not all lowercase and already in the path
+                    if path.clone().iter().fold(0, |acc, x| if x == &point && x.chars().all(|c| matches!(c, 'a'..='z')) {acc + 1} else {acc}) == 0 {
+                        changes += 1;
+                        let mut new_path = path.clone();
+                        new_path.push(point);
+                        paths.push(new_path);
+                    }
+                }
+            }
+        }
+
+        // Break if no changes done in this iteration and place tmp back into paths
+        if changes == 0 {
+            //paths.append(&mut tmp);
+            break;
+        };
     }
 }
 
