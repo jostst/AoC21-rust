02d647a3b8133bc6b04bd5e778a7804ffcad5f8b
Jošt Stergar <jost.stergar@fmf.uni-lj.si>
2021-12-14 11:33:28 +0100

    Pt2 day14 using classes works!

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -25,17 +25,52 @@ fn part_one(data: &Vec<char>, instructions: &Vec<(char, char, char)>) -> i32 {
     calculate_hash(&polymer)
 }
 
-/// This is part one function
-fn part_two(data: &Vec<char>, instructions: &Vec<(char, char, char)>) -> i32 {
-    let mut polymer = data.to_owned();
+fn part_two(data: &Vec<char>, instructions: &Vec<(char, char, char)>) -> u64 {
+    // first index is first letter, second idx is second letter
+    let mut pairs: Vec<Vec<u64>> = vec![vec![0;26];26];
     
-    // Iterate 10 times
-    for i in 0..40 {
-        polymer = grow_polymer(&polymer, &instructions);
-        println!("Iteration: {}", i);
+    // Fill the pair matrix from the data
+    for i in 0..data.len()-1 {
+        pairs[idx(&data[i])][idx(&data[i+1])] += 1;
     }
 
-    calculate_hash(&polymer)
+    // Iterate polymer synthesis
+    for _ in 0..40 {
+        let prev = pairs.clone();
+        for instruction in instructions {
+            let tmp = prev[idx(&instruction.0)][idx(&instruction.1)];
+            if tmp > 0 {
+                pairs[idx(&instruction.0)][idx(&instruction.1)] -= tmp;
+                pairs[idx(&instruction.0)][idx(&instruction.2)] += tmp;
+                pairs[idx(&instruction.2)][idx(&instruction.1)] += tmp;
+            }
+        }
+    }
+
+    // Calculate occurances
+    let mut occurances: Vec<u64> = vec![0; 26];
+    for (i, line) in pairs.clone().iter().enumerate() {
+        occurances[i] = line.clone().iter().fold(0, |acc, &x| acc + x);
+    }
+    occurances[idx(data.iter().last().unwrap())] += 1;
+
+    for line in &pairs {
+        println!("{:?}", line);
+    }
+    println!("[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]");
+    println!("\n{:?}", &occurances);
+    
+    // Calculate the metric
+    let max = occurances.clone().into_iter().max().unwrap();
+    let mut min = max;
+    for i in occurances {
+        if i > 0 && i < min {min = i;};
+    }
+    max - min
+}
+
+fn idx(c: &char) -> usize {
+    (*c as i32 - 65) as usize
 }
 
 /// This grows polymer according to the instructions
@@ -114,4 +149,54 @@ fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Result::Ok(io::BufReader::new(file).lines())
+}
+
+
+/// This is part one function
+fn part_two_recursive(data: &Vec<char>, instructions: &Vec<(char, char, char)>) -> i32 {
+    let polymer = data.to_owned();
+    
+    let mut occ: Vec<i32> = vec![0;25];
+
+    for c in data {
+        occ[(*c as i32 - 65) as usize] += 1;
+    }
+
+    for i in 0..polymer.len()-1{
+        println!("New pair!");
+        let new_occ = grow_rec(polymer[i], polymer[i+1], &instructions, 0);
+        for j in 0..occ.len() {occ[j] += new_occ[j];};
+    }
+
+    let max = occ.clone().into_iter().max().unwrap();
+    let mut min = max;
+    for i in occ {
+        if i > 0 && i < min {min = i;};
+    }
+    max - min
+}
+
+/// Try recursion...
+fn grow_rec(a: char, b:char, instructions: &Vec<(char, char, char)>, lim: i32) -> Vec<i32> {
+    let mut occ = vec![0;25];
+    println!("Depth: {}", lim);
+    if lim < 40{
+        // If instruction exists for i,i+1 pair perform the insertion
+        for instruction in instructions {
+            if a == instruction.0 && b == instruction.1 {
+                occ[(instruction.2 as i32 - 65) as usize] += 1;
+
+                let occ1 = grow_rec(a, instruction.2, instructions, lim+1);
+                let occ2 = grow_rec(instruction.2, b, instructions, lim+1);
+
+                for i in 0..occ.len() {
+                    occ[i] += occ1[i] + occ2[i];
+                }
+                break;
+            }
+        }
+    } else {
+
+    }
+    return occ;
 }
