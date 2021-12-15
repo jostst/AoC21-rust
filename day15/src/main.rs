fcf10f277bf31c9832d5d199d2f2765d47285217
Jošt Stergar <jost.stergar@fmf.uni-lj.si>
2021-12-15 15:23:33 +0100

    Removed majority of casts

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -13,11 +13,6 @@ fn main() {
     };
 }
 
-fn get_data(data: &Vec<Vec<i32>>, i: i32, j: i32) -> i32 {
-    let orig = (data.len() as i32, data[0].len() as i32);
-    ((data[(i%orig.0) as usize][(j%orig.1) as usize] + i/orig.0 + j/orig.1)-1)%9 + 1
-}
-
 /// Dijkstra's algorithm
 fn dijkstra(data: &Vec<Vec<i32>>, inflation: i32) -> i32 {
     let size = (data.len()*inflation as usize, data[0].len()*inflation as usize);
@@ -28,27 +23,27 @@ fn dijkstra(data: &Vec<Vec<i32>>, inflation: i32) -> i32 {
 
     // Smarter queue
     let mut visited: Vec<Vec<bool>> = vec![vec![false; size.1]; size.0];
-    let mut queue: Vec<(i32, i32)> = vec![];
+    let mut queue: Vec<(usize, usize)> = vec![];
     queue.push((0,0));
 
     // While nodes are still to be visited, repeat
     while queue.len() > 0 as usize {
         // Find current node with minimal distance
         let mut dist = i32::MAX;
-        let mut current: (i32, i32) = (0, 0);
+        let mut current: (usize, usize) = (0, 0);
         for node in &queue {
-            if distances[node.0 as usize][node.1 as usize] < dist {
-                dist = distances[node.0 as usize][node.1 as usize];
+            if distances[node.0][node.1] < dist {
+                dist = distances[node.0][node.1];
                 current = *node;
             }
         }
 
         // Update distances of the neighbour nodes, if needed, and add them to the queue
         for neighbour in unvisited_neighbours(current, &visited) {
-            let tmp = distances[current.0 as usize][current.1 as usize] + 
-                        get_data(&data, neighbour.0 as i32, neighbour.1 as i32);
-            if tmp < distances[neighbour.0 as usize][neighbour.1 as usize] {
-                distances[neighbour.0 as usize][neighbour.1 as usize] = tmp;
+            let tmp = distances[current.0][current.1] + 
+                        get_data(&data, neighbour.0, neighbour.1);
+            if tmp < distances[neighbour.0][neighbour.1] {
+                distances[neighbour.0][neighbour.1] = tmp;
             }
             // Add neighbour to the queue
             if !queue.contains(&neighbour) {queue.push(neighbour);}
@@ -56,25 +51,35 @@ fn dijkstra(data: &Vec<Vec<i32>>, inflation: i32) -> i32 {
 
         // Remove current node from unvisited and mark current visited
         queue.remove(*&queue.iter().position(|x| *x == current).unwrap());
-        visited[current.0 as usize][current.1 as usize] = true;
+        visited[current.0][current.1] = true;
 
         // If the destination node was visited, break -> we win
-        if current == ((size.0-1) as i32, (size.1-1) as i32) {break;} 
+        if current == (size.0-1, size.1-1) {break;} 
     }
     distances[distances.len()-1][distances[0].len()-1]
 }
 
-fn unvisited_neighbours(point: (i32, i32), visited: &Vec<Vec<bool>>) -> Vec<(i32, i32)> {
-    let mut neighbours: Vec<(i32, i32)> = Vec::new();
-    let mut size = (visited.len() as i32, visited[0].len());
+/// Get data. Takes data inflation for part 2 into account
+fn get_data(data: &Vec<Vec<i32>>, i: usize, j: usize) -> i32 {
+    let i: i32 = i as i32;
+    let j: i32 = j as i32;
+    let orig = (data.len() as i32, data[0].len() as i32);
+    ((data[(i%orig.0) as usize][(j%orig.1) as usize] + i/orig.0 + j/orig.1)-1)%9 + 1
+}
+
+/// Returns a vector of viable neighbours for Dijkstra. Eliminates invalid idxs and visited
+/// neighbours
+fn unvisited_neighbours(point: (usize, usize), visited: &Vec<Vec<bool>>) -> Vec<(usize, usize)> {
+    let mut neighbours: Vec<(usize, usize)> = Vec::new();
+    let size = (visited.len(), visited[0].len());
 
     for neighbour in NEIGHBOURS {
-        let n = (point.0+neighbour.0, point.1+neighbour.1);
+        let n: (i32, i32) = (point.0 as i32 + neighbour.0, point.1 as i32 + neighbour.1);
 
         if n.0 >= 0 && n.1 >= 0 && n.0 < size.0 as i32 && n.1 < size.1 as i32{
             // Check if not visited
             if !visited[n.0 as usize][n.1 as usize] {
-                neighbours.push((n.0, n.1));
+                neighbours.push((n.0 as usize, n.1 as usize));
             }
         }
     }
